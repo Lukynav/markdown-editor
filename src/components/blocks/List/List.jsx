3b3204a3dba1b4028b8b79073425d24892c8d9ae
Lucas Navarro <Lucasnav6@gmail.com>
2022-11-08 20:38:42 -0300

    ✨ add link support

--- src/components/blocks/List/List.jsx
+++ src/components/blocks/List/List.jsx
@@ -1,7 +1,15 @@
+import { paragraphSorter } from "../../../utils/paragraphSorter"
+
 export const List = ({ index, text }) => {
+  const content = text.slice(1)
+  const paragraph = paragraphSorter(content)
     return (
       <li className='ml-4' key={index}>
-        {text.slice(1)}
+        {paragraph.map(e => {
+        if (e.type == 'text') return e.content
+        if (e.type == 'bold') return (<b className='text-bold text-gray-200'>{e.content}</b>)
+        if (e.type == 'link') return (<a href={e.url} className='text-blue-400 font-bold'>{e.content}</a>)
+      })}
       </li>
     )
   }
