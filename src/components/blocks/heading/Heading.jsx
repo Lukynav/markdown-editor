55ad6cf8991c8961c5bda93e858334cb1c2aaa8f
Lucas Navarro <Lucasnav6@gmail.com>
2022-11-08 19:26:52 -0300

    💄 update the heading style

--- src/components/blocks/heading/Heading.jsx
+++ src/components/blocks/heading/Heading.jsx
@@ -4,8 +4,8 @@ export const Heading = ({ index, text }) => {
   return (
     <div className='mb-2 text-white' index={index}>
       {
-                (headingIndex == 1) && <h1 className='text-3xl border-b-2 border-white mb-4'>{text}</h1> ||
-                (headingIndex == 2) && <h2 className='text-2xl mt-6'>{text}</h2> ||
+                (headingIndex == 1) && <h1 className='text-3xl border-b-2 border-white mb-4 font-bold'>{text}</h1> ||
+                (headingIndex == 2) && <h2 className='text-2xl mt-6 font-bold'>{text}</h2> ||
                 (headingIndex == 3) && <h3 className='text-xl mt-6'>{text}</h3> ||
                 (headingIndex == 4) && <h4 className='text-lg mt-6'>{text}</h4> ||
                 (headingIndex == 5) && <h5 className='text-md mt-6'>{text}</h5> ||
